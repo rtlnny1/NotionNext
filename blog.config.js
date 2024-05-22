5e7ad4a79a381eda73eca0ca5b4ab2ab137a7e56
rtlnny1 <148055517+rtlnny1@users.noreply.github.com>
2024-05-22 14:37:14 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -25,7 +25,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Lin', // 您的昵称
   BIO: process.env.NEXT_PUBLIC_BIO || '所有你乐于挥霍的时间, 都不能算作浪费', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://notion-next-theta-ebon.vercel.app/', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客, Lin, Blog, Lin's Blog', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客, Lin, Blog', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'rtlin4@gmail.com', // 邮箱地址 例如mail@tangly1024.com
