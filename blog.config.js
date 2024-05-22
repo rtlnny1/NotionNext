b7ece690cf80355f337687a61799a6e0f92467a8
rtlnny1 <148055517+rtlnny1@users.noreply.github.com>
2024-05-22 14:31:34 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -18,9 +18,7 @@ const BLOG = {
 
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
   GREETING_WORDS:
-    process.env.NEXT_PUBLIC_GREETING_WORDS || '一个程序员,干饭人,二次元, 欢迎来到我的博客🎉',
-    '所有你乐于挥霍的时间, 都不能算作浪费',
-    '智慧, 自由, 和爱',
+    process.env.NEXT_PUBLIC_GREETING_WORDS || '一个程序员，欢迎来到我的博客🎉, 所有你乐于挥霍的时间，都不能算作浪费, 智慧，自由，和爱',
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
